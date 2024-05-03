ae66f2b82bc7c3af78a7e33e019e510e7a86d7cc
Rodrigo Blanco <rodrigob_conexiondigital@hotmail.com>
2024-05-03 00:15:12 -0300

    First commit

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -14,5 +14,6 @@ messageRoutes.get("/messages/:ticketId", isAuth, MessageController.index);
 messageRoutes.post("/messages/:ticketId", isAuth, upload.array("medias"), MessageController.store);
 messageRoutes.delete("/messages/:messageId", isAuth, MessageController.remove);
 messageRoutes.post("/api/messages/send", tokenAuth, upload.array("medias"), MessageController.send);
+messageRoutes.get("/api/messages/:ticketId", tokenAuth, MessageController.index);
 
 export default messageRoutes;
